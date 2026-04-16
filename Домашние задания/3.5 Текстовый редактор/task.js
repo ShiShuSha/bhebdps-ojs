47be8a3730c702f13e4c94e188e499af0df931f1
ShiShuSha <malgash.ss@gmail.com>
2026-04-16 18:45:53 +0500

    Update task.js

--- Домашние задания/3.5 Текстовый редактор/task.js
+++ Домашние задания/3.5 Текстовый редактор/task.js
@@ -1,18 +1,18 @@
 const editor = document.getElementById('editor');
 const clearBtn = document.getElementById('clear');
 
-// Восстановление текста
+// восстановление текста
 const savedText = localStorage.getItem('editorText');
 if (savedText) {
   editor.value = savedText;
 }
 
-// Сохранение текста
+// сохранение текста
 editor.addEventListener('input', () => {
   localStorage.setItem('editorText', editor.value);
 });
 
-// Очистка текста
+// очистка текста
 clearBtn.addEventListener('click', () => {
   editor.value = '';
   localStorage.removeItem('editorText');
